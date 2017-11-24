90997e08451bf37f351793bd1e451f148e847415
Jesse Pinnell (Desktop) <jesse.pinnell@efi.com>
2017-11-23 22:10:17 -0500

    Remove equipment accessories from exercise db
    Add muscle_groups and stretches tables
    Reclassify some muscles into different groups

--- database/exercise_db/make_db.sh
+++ database/exercise_db/make_db.sh
@@ -8,10 +8,11 @@ SCRIPT_DIR=tests/database/exercise_db
 
 # need to preserve order here or foreign keys will die
 rm -f $1
-sqlite3 $1 < ${SCRIPT_DIR}/create.sqlite
-sqlite3 $1 < ${SCRIPT_DIR}/muscles.sqlite
-sqlite3 $1 < ${SCRIPT_DIR}/fixtures.sqlite
-sqlite3 $1 < ${SCRIPT_DIR}/equipment_accessories.sqlite
-sqlite3 $1 < ${SCRIPT_DIR}/equipment_rigs.sqlite
-sqlite3 $1 < ${SCRIPT_DIR}/exercises.sqlite
+sqlite3 $1 < ${SCRIPT_DIR}/create.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/muscle_groups.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/muscles.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/stretches.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/fixtures.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/equipment_rigs.sqlite && \
+sqlite3 $1 < ${SCRIPT_DIR}/exercises.sqlite && \
 sqlite3 $1 < ${SCRIPT_DIR}/muscles_exercised.sqlite

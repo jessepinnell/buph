c8b9a87aa929dbc8b02d3208d0ad1a9956dd30c8
Jesse Pinnell (Desktop) <jesse.pinnell@efi.com>
2018-01-14 14:03:40 -0500

    Add remaining YAML types

--- scripts/process_yaml.py
+++ scripts/process_yaml.py
@@ -32,14 +32,15 @@ import yaml
 class ExerciseYAML(yaml.YAMLObject):
     yaml_tag = u'!exercise'
 
-
     def __init__(self, data):
         self.abbreviation = ""
+        self.variation_of = ""
         self.aka = []
         self.muscles = {}
+        self.steps = []
         self.info = {}
-        self.fixtures = []
-        self.rigs = []
+        self.fixtures = {}
+        self.rigs = {}
 
         for exercise in data:
             if exercise[0].tag == 'tag:yaml.org,2002:merge':
@@ -51,33 +52,34 @@ class ExerciseYAML(yaml.YAMLObject):
     def setMembers(self, key, value):
         if key == 'abbreviation':
             self.abbreviation = value
+        elif key == 'variation of':
+            self.variation_of = value
         elif key == 'aka':
-            # Append to merged akas (if any)
-            #for aka in value:
-            #    self.aka.append(aka.value)
-
             # overwrite merged akas (if any)
-            self.aka = [aka.value for aka in value]
+            self.aka = {aka.value for aka in value}
         elif key == 'muscles':
             for muscle in value:
                 self.muscles[muscle.value[0][0].value] = float(muscle.value[0][1].value)
         elif key == 'info':
             for info in value:
                 self.info[info.value[0][0].value] = info.value[0][1].value
+        elif key == 'steps':
+            self.steps = [step.value for step in value]
         elif key == 'fixtures':
-            self.fixtures = [fixture.value for fixture in value]
+            self.fixtures = {fixture.value for fixture in value}
         elif key == 'rigs':
-            for rigs in value:
-                self.rigs.append(rigs.value)
+            self.rigs = {rig.value for rig in value}
         else:
             print(f"Unknown key: {key}")
 
 
     def __str__(self):
         return f"abbreviation: {self.abbreviation}\n" +\
+            f"variation of: {self.variation_of}\n" +\
             f"aka: {self.aka}\n" +\
             f"muscles : {self.muscles}\n" +\
             f"info: {self.info}\n" +\
+            f"steps: {self.steps}\n" +\
             f"fixtures: {self.fixtures}\n" +\
             f"rigs: {self.rigs}\n"
 
@@ -87,10 +89,25 @@ class ExerciseYAML(yaml.YAMLObject):
 
 class RigYAML(yaml.YAMLObject):
     yaml_tag = u'!rig'
-    info = None
 
     def __init__(self, data):
-        pass
+        self.info = ""
+
+        for rig in data:
+            if rig[0].tag == 'tag:yaml.org,2002:merge':
+                for merged_rigs in rig[1].value:
+                    self.setMembers(merged_rigs[0].value, merged_rigs[1].value)
+            else:
+                self.setMembers(rig[0].value, rig[1].value)
+
+    def setMembers(self, key, value):
+        if key == 'info':
+            self.info = value
+        else:
+            print(f"Unknown key: {key}")
+
+    def __str__(self):
+        return f"info: {self.info}\n"
 
     @classmethod
     def from_yaml(cls, loader, node):
@@ -98,12 +115,40 @@ class RigYAML(yaml.YAMLObject):
 
 class MuscleYAML(yaml.YAMLObject):
     yaml_tag = u'!muscle'
-    group = ""
-    info = []
-    antagonists = {}
 
     def __init__(self, data):
-        pass
+        self.group = ""
+        self.fma = None
+        self.info = {}
+        self.antagonists = {}
+
+        for muscle  in data:
+            if muscle[0].tag == 'tag:yaml.org,2002:merge':
+                for merged_muscles in muscles[1].value:
+                    self.setMembers(merged_muscles[0].value, merged_muscles[1].value)
+            else:
+                self.setMembers(muscle[0].value, muscle[1].value)
+
+    def setMembers(self, key, value):
+        if key == 'group':
+            self.group = value
+        elif key == 'fma':
+            self.fma = int(value)
+        elif key == 'info':
+            if isinstance(value, list):
+                for info in value:
+                    self.info[info.value[0][0].value] = info.value[0][1].value
+        elif key == 'antagonists':
+            if isinstance(value, list):
+                self.antagonists = {antagonist[0].value for antagonist in value}
+        else:
+            print(f"Unknown key: {key}")
+
+    def __str__(self):
+        return f"group: {self.group}\n" +\
+           f"FMA: {self.fma}\n" +\
+           f"info: {self.info}\n" +\
+           f"antagonists: {self.antagonists}"
 
     @classmethod
     def from_yaml(cls, loader, node):
@@ -111,12 +156,58 @@ class MuscleYAML(yaml.YAMLObject):
 
 class StretchYAML(yaml.YAMLObject):
     yaml_tag = u'!stretch'
-    aka = []
-    desc = []
-    info = []
 
     def __init__(self, data):
-        pass
+        self.abbreviation = ""
+        self.variation_of = ""
+        self.aka = {}
+        self.muscles = {}
+        self.info = {}
+        self.steps = []
+        self.fixtures = {}
+        self.rigs = {}
+
+        for exercise in data:
+            if exercise[0].tag == 'tag:yaml.org,2002:merge':
+                for merged_exercise in exercise[1].value:
+                    self.setMembers(merged_exercise[0].value, merged_exercise[1].value)
+            else:
+                self.setMembers(exercise[0].value, exercise[1].value)
+
+    def setMembers(self, key, value):
+        if key == 'abbreviation':
+            self.abbreviation = value
+        elif key == 'variation of':
+            self.variation_of = value
+        elif key == 'aka':
+            # overwrite merged akas (if any)
+            self.aka = {aka.value for aka in value}
+        elif key == 'muscles':
+            for muscle in value:
+                self.muscles[muscle.value[0][0].value] = float(muscle.value[0][1].value)
+        elif key == 'info':
+            for info in value:
+                self.info[info.value[0][0].value] = info.value[0][1].value
+        elif key == 'fixtures':
+            self.fixtures = {fixture.value for fixture in value}
+        elif key == 'steps':
+            self.steps = {fixture.value for fixture in value}
+        elif key == 'rigs':
+            self.rigs = {rig.value for rig in value}
+        else:
+            print(f"Unknown key: {key}")
+
+
+    def __str__(self):
+        return f"abbreviation: {self.abbreviation}\n" +\
+            f"variation of: {self.variation_of}\n" +\
+            f"aka: {self.aka}\n" +\
+            f"muscles : {self.muscles}\n" +\
+            f"info: {self.info}\n" +\
+            f"steps: {self.steps}\n" +\
+            f"fixtures: {self.fixtures}\n" +\
+            f"rigs: {self.rigs}\n"
+
 
     @classmethod
     def from_yaml(cls, loader, node):
@@ -125,15 +216,29 @@ class StretchYAML(yaml.YAMLObject):
 
 class FixtureYAML(yaml.YAMLObject):
     yaml_tag = u'!fixture'
-    info = []
 
     def __init__(self, data):
-        pass
+        self.info = ""
+
+        for fixture in data:
+            if fixture[0].tag == 'tag:yaml.org,2002:merge':
+                for merged_fixtures in fixture[1].value:
+                    self.setMembers(merged_fixtures[0].value, merged_fixtures[1].value)
+            else:
+                self.setMembers(fixture[0].value, fixture[1].value)
+
+    def setMembers(self, key, value):
+        if key == 'info':
+            self.info = value
+        else:
+            print(f"Unknown key: {key}")
+
+    def __str__(self):
+        return f"info: {self.info}\n"
 
     @classmethod
     def from_yaml(cls, loader, node):
         return FixtureYAML(node.value)
-
 
 
 def process():
@@ -142,9 +247,6 @@ def process():
     app_args_parser.add_argument('--dbtype', choices=['postgres', 'sqlite3'], default='sqlite3')
     app_args_parser.add_argument('files', nargs=argparse.REMAINDER)
     app_args = app_args_parser.parse_args()
-
-    fixtures = {}
-    rigs = {}
 
     yaml.SafeLoader.add_constructor('!exercise', ExerciseYAML.from_yaml)
     yaml.SafeLoader.add_constructor('!muscle', MuscleYAML.from_yaml)
@@ -181,6 +283,17 @@ def process():
     for key, exercise in exercises.items():
         print(f"\033[32m{key}\033[0m:\n{exercise}")
 
+    for key, fixture in fixtures.items():
+        print(f"\033[31m{key}\033[0m:\n{fixture}")
+
+    for key, rig in rigs.items():
+        print(f"\033[33m{key}\033[0m:\n{rig}")
+
+    for key, muscle in muscles.items():
+        print(f"\033[35m{key}\033[0m:\n{muscle}")
+
+    for key, stretch in stretches.items():
+        print(f"\033[36m{key}\033[0m:\n{stretch}")
 
 if __name__ == "__main__":
     process()

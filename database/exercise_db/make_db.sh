8cf4f1c10931ad1a5c0daeb0becd8819be92b221
Jesse Pinnell (Desktop) <jesse.pinnell@efi.com>
2018-01-14 16:11:28 -0500

    Create test db from yaml

--- database/exercise_db/make_db.sh
+++ database/exercise_db/make_db.sh
@@ -4,26 +4,10 @@ if [ $# -ne 1 ]; then
 	echo "Usage: $0 [db filename]"
 	exit -1
 fi
-SCRIPT_DIR=tests/database/exercise_db
 DB_FILENAME=$1
 
-function source_sql
-{
-	sqlite3 ${DB_FILENAME} < ${SCRIPT_DIR}/${1} || { echo "Error in ${1}"; exit -1; }
-}
-
-# need to preserve order here or foreign keys will die
 rm -f $1
 
-source_sql create.sqlite
-sqlite3 ${DB_FILENAME} < /tmp/testo.sqlite || { echo "Error in ${1}"; exit -1; }
-#source_sql muscle_groups.sqlite
-#source_sql muscles.sqlite
-#source_sql stretches.sqlite
-#source_sql fixtures.sqlite
-#source_sql rigs.sqlite
-#source_sql exercises.sqlite
-#source_sql muscles_exercised.sqlite
-#source_sql muscle_antagonists.sqlite
-#source_sql exercise_rigs.sqlite
-#source_sql exercise_fixtures.sqlite
+sqlite3 ${DB_FILENAME} < tests/database/exercise_db/create.sqlite
+python3 scripts/process_yaml.py --format sqlite3 tests/data/exercise_data/*.yaml > tests/data/exercise_data/test.sqlite
+sqlite3 ${DB_FILENAME} < tests/data/exercise_data/test.sqlite

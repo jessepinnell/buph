6299e5a2111377d413a11969d21c3b857d743602
Jesse Pinnell (Desktop) <jesse.pinnell@efi.com>
2018-03-11 09:33:33 -0400

    Add initialization of postgres data

--- scripts/make_base_exercise_db_postgres.sh
+++ scripts/make_base_exercise_db_postgres.sh
@@ -12,10 +12,17 @@ DB_USERNAME=$1
 # This requires the local user to have the following privs: CREATEDB
 
 # Create database
+echo "Dropping postgres xrsrv table..."
 dropdb --if-exists xrsrv
+
+echo "Creating postgres xrsrv table..."
 createdb xrsrv
-psql xrsrv ${DB_USERNAME} < tests/data/exercise_data/create.sql
 
-#python3 python/utils/process_yaml.py --format postgres tests/data/exercise_data/*.yaml tests/data/exercise_data/exercises/*.yaml > tests/data/exercise_data/test.sql
+echo "Creating postgres tables..."
+psql xrsrv -q ${DB_USERNAME} < tests/data/exercise_data/create.sql
 
-#psql ${DB_FILENAME} < tests/data/exercise_data/test.sql
+echo "Generating postgres data..."
+python3 python/utils/process_yaml.py --format postgres tests/data/exercise_data/*.yaml tests/data/exercise_data/exercises/*.yaml > tests/data/exercise_data/test.sql
+
+echo "Inserting postgres data..."
+psql xrsrv -q ${DB_USERNAME} < tests/data/exercise_data/test.sql
--- scripts/make_base_exercise_db_sqlite.sh
+++ scripts/make_base_exercise_db_sqlite.sh
@@ -8,6 +8,11 @@ DB_FILENAME=$1
 
 rm -f $1
 
+echo "Creating sqlite tables..."
 sqlite3 ${DB_FILENAME} < tests/data/exercise_data/create.sqlite
+
+echo "Generating sqlite data..."
 python3 python/utils/process_yaml.py --format sqlite3 tests/data/exercise_data/*.yaml tests/data/exercise_data/exercises/*.yaml > tests/data/exercise_data/test.sqlite
+
+echo "Inserting sqlite data..."
 sqlite3 ${DB_FILENAME} < tests/data/exercise_data/test.sqlite
